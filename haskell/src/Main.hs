24972ceb73694814cac814739dfd50b04af23fbf
FLuiz22 <filipeluiz39@gmail.com>
2024-08-28 16:48:29 -0300

    feat: mover tropas adicionado

--- haskell/src/Main.hs
+++ haskell/src/Main.hs
@@ -4,10 +4,14 @@ import TerritorioSrv
 import Territorio
 import Tropa
 
+
+
 main :: IO ()
 main = do
     let tropa = Tropa { jogador = 1 }
     let ter = Territorio { nome = "T1", tropas = [tropa], vizinhos = []}
+    let ter2 = Territorio { nome = "T2", tropas = [tropa], vizinhos = []}
+    let ter2N = moverTropa ter ter2 1
 
-    print(length (tropas (removeTropa ter)))
-    print(length (tropas (adicionaTropa ter [tropa])))
+    print(length (tropas (fst (ter2N))))
+    print(length (tropas (snd (ter2N))))
