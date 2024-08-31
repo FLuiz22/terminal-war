6f880b5480c36960f2c332f4dada14222db6ab66
alinebmr <alinebrito.maracaja@gmail.com>
2024-08-31 14:14:06 -0300

    correcaoMergeNovamente

--- haskell/src/JogoCtrl.hs
+++ haskell/src/JogoCtrl.hs
@@ -9,25 +9,6 @@ import Jogo
 import JogoSrv
 import Data.Maybe
 
-
--- verficaTropaJg :: Tropa -> Int -> Bool
--- verficaTropaJg tropa idJogador = if idJogador == (jogador tropa) then True else False
-
-verficaVizinhos :: String -> [String] -> Bool
-verficaVizinhos _ [] = False
-verficaVizinhos nomeTer (x:xs)
-    | nomeTer == x = True
-    | otherwise = verficaVizinhos nomeTer xs
-
--- Retorno da função temporário
--- moverTropaCtrl :: Territorio -> Territorio -> Int -> Int -> Maybe [Territorio]
--- moverTropaCtrl ter1 ter2 qntd idJogador = if (length (tropas ter1)) - qntd <= 0 || 
---     not (verficaVizinhos (nome ter2) (vizinhos ter1)) || not (verficaTropaJg (head (tropas ter2)) idJogador)
---     then Nothing
-
---     else Just (moverTropa ter1 ter2 qntd)
-
-
 -- Verifica caso um jogador já tenha vencido, se sim, o jogador será retornado, se nao, será retornado Nothing
 verificaVitoriaCtrl :: Jogador -> Jogador -> Maybe Jogador
 verificaVitoriaCtrl jogador1 jogador2 = verificaVitoria jogador1 jogador2
@@ -39,3 +20,6 @@ startGameCtrl = startGame
 -- Funcao que retorna os territorios de um jogador especifico, 1 ou 2
 achaTerritoriosDeJogadorCtrl :: Jogo ->  Int -> [String]
 achaTerritoriosDeJogadorCtrl jogo jogador = achaTerritoriosDeJogador jogo jogador
+
+moverTropaCtrl :: Territorio -> Territorio -> Int -> Bool
+moverTropaCtrl ter1 ter2 qntd = isJust (moverTropaSrv ter1 ter2 qntd)
