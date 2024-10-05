819558c412a94796e7394896e320e2ce69b9a36d
FLuiz22 <filipeluiz39@gmail.com>
2024-10-05 13:18:01 -0300

    Feat/Refactor: Mover tropa finalizado e correções

--- prolog/src/JogoCtrl.pl
+++ prolog/src/JogoCtrl.pl
@@ -1,10 +1,14 @@
+:- consult(['../structs.pl', './JogoSrv.pl']).
+
 
 /* mover_tropa_ctrl: Função para deslocar as tropas de um território a outro, desde
                       que cumpra as restrições. A função devolve uma lista contendo
                       os novos territórios após o deslocamento das tropas. */
 
 
-mover_tropa_ctrl(TerritorioOrigem, TerritorioDestino, NumTropas, NovosTerritorios) :-
-    mover_tropa_srv(TerritorioOrigem, TerritorioDestino, NumTropas, Resultado),
-    (Resultado \= none -> NovosTerritorios = Resultado ;
-    NovosTerritorios = [] ).
+mover_tropa_ctrl(TerritorioOrigem, TerritorioDestino, NumTropas, Ter) :-
+    get_global_variable(territorios, Territorios),
+    get_dict(TerritorioOrigem, Territorios, V1),
+    V1 - NumTropas >= 1,
+    mover_tropa_srv(Territorios, TerritorioOrigem, TerritorioDestino, NumTropas, Resultado),
+    set_global_variable(territorios, Resultado).
